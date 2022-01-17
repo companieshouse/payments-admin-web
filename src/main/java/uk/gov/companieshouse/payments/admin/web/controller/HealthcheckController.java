a3f8c6a0dbb88054b8e7de5d0f3b639dad955252
Rohan Sachdev <rsachdev@companieshouse.gov.uk>
2022-01-17 09:38:56 +0000

    Add controller with healthcheck for skeleton app

---
+++ src/main/java/uk/gov/companieshouse/payments/admin/web/controller/HealthcheckController.java
@@ -0,0 +1,23 @@
+package uk.gov.companieshouse.payments.admin.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Healthcheck controller returns a 200 response if the service is running.
+ * <p>The ResponseEntity accepts a wildcard and thus a more verbose healthcheck can be written
+ * to query the status of mongo/kafka/disk space etc. The results can be marshalled into
+ * an object and passed to the ResponseEntity constructor along with the HttpStatus.
+ * <br>This will ultimately act as a more lightweight spring actuator.
+ */
+@Controller
+public class HealthcheckController {
+
+    @GetMapping(value = "/payment-admin-web/healthcheck")
+    public ResponseEntity<?> performHealthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+}
