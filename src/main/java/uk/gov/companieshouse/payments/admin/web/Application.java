1fa15778f3850e290ccfe5cc9b413c14cb340153
Edwin Mead <emead@companieshouse.gov.uk>
2022-02-16 15:21:56 +0000

    Add check for Bulk Refund Admin privilege BI-10066

--- src/main/java/uk/gov/companieshouse/payments/admin/web/Application.java
+++ src/main/java/uk/gov/companieshouse/payments/admin/web/Application.java
@@ -1,15 +1,35 @@
 package uk.gov.companieshouse.payments.admin.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.gov.companieshouse.payments.admin.web.interceptor.UserDetailsInterceptor;
+import uk.gov.companieshouse.payments.admin.web.interceptor.UserPermissionInterceptor;
 
 @SpringBootApplication
-public class Application {
+public class Application implements WebMvcConfigurer{
 
 	public static final String APPLICATION_NAME_SPACE = "payments-admin-web";
 
+	private UserPermissionInterceptor userPermissionInterceptor;
+	private UserDetailsInterceptor userDetailsInterceptor;
+
+	@Autowired
+	public Application(UserPermissionInterceptor userPermissionInterceptor, UserDetailsInterceptor userDetailsInterceptor) {
+		this.userPermissionInterceptor = userPermissionInterceptor;
+		this.userDetailsInterceptor = userDetailsInterceptor;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(userPermissionInterceptor);
+		registry.addInterceptor(userDetailsInterceptor);
 	}
 
 }
---
+++ src/main/java/uk/gov/companieshouse/payments/admin/web/interceptor/UserPermissionInterceptor.java
@@ -0,0 +1,31 @@
+package uk.gov.companieshouse.payments.admin.web.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import uk.gov.companieshouse.payments.admin.web.session.SessionService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Component
+public class UserPermissionInterceptor implements HandlerInterceptor{
+
+    @Autowired
+    private SessionService sessionService;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+
+        Map<String, Object> userPermissions = sessionService.getUserPermissions();
+
+        Integer refundPermission = (Integer) userPermissions.get("/admin/bulk-refunds");
+
+        if (modelAndView != null && (refundPermission == null || refundPermission != 1)) {
+            modelAndView.setViewName("refunds/pageNotFound");
+        }
+    }
+}
