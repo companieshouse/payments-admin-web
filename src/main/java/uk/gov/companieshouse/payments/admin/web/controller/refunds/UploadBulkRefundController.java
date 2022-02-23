dec8b9659d3abc6e04dfb132fbbae3dcd32564a1
jphillips2121 <jphillips3@companieshouse.gov.uk>
2022-02-23 08:13:23 +0000

    Remove wildcard import

--- src/main/java/uk/gov/companieshouse/payments/admin/web/controller/refunds/UploadBulkRefundController.java
+++ src/main/java/uk/gov/companieshouse/payments/admin/web/controller/refunds/UploadBulkRefundController.java
@@ -3,7 +3,11 @@ package uk.gov.companieshouse.payments.admin.web.controller.refunds;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import uk.gov.companieshouse.payments.admin.web.controller.BaseController;
 import uk.gov.companieshouse.payments.admin.web.models.UploadRefundFile;
 
