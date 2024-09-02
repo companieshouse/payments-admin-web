4b7904f844ea62b94ca623aa758989890b399329
NeilShadrach <neil.shadrach@corryn.com>
2024-09-02 13:59:01 +0100

    JU-566 Update web security

---
+++ src/main/java/uk/gov/companieshouse/payments/admin/web/WebSecurity.java
@@ -0,0 +1,34 @@
+package uk.gov.companieshouse.payments.admin.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import uk.gov.companieshouse.auth.filter.HijackFilter;
+import uk.gov.companieshouse.auth.filter.UserAuthFilter;
+import uk.gov.companieshouse.session.handler.SessionHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain healthcheckSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.securityMatcher("/admin/payments/healthcheck")
+            .csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain apiKeyPaymentsAdminWebSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/admin/payments/refunds", "/admin/payments/summary", "/")
+            .csrf().disable()
+            .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
+            .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+            .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
+        return http.build();
+    }
+}
