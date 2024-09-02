6084c0eefe652db517447ec98f6c3d085284baad
NeilShadrach <neil.shadrach@corryn.com>
2024-09-02 11:56:46 +0100

    JU-566 Replace HandlerInterceptorAdapter with HandlerInterceptor

--- src/main/java/uk/gov/companieshouse/payments/admin/web/interceptor/LoggingInterceptor.java
+++ src/main/java/uk/gov/companieshouse/payments/admin/web/interceptor/LoggingInterceptor.java
@@ -3,7 +3,7 @@ package uk.gov.companieshouse.payments.admin.web.interceptor;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 import uk.gov.companieshouse.logging.util.RequestLogger;
@@ -15,7 +15,7 @@ import static uk.gov.companieshouse.payments.admin.web.Application.APPLICATION_N
 
 
 @Component
-public class LoggingInterceptor extends HandlerInterceptorAdapter implements RequestLogger {
+public class LoggingInterceptor implements HandlerInterceptor, RequestLogger {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(APPLICATION_NAME_SPACE);
 
